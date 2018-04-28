76f5466744b4e58ee3b6d1ee26494f0a832c9052
Ryan Hill <rdh35@duke.edu>
2018-04-28 13:41:44 -0400

    Added line to fix 413 error

--- src/server/main.js
+++ src/server/main.js
@@ -22,6 +22,8 @@ app.use(Cors())
 app.use(BodyParser.urlencoded({ extended: true }))
 // parse JSON form data
 app.use(BodyParser.json())
+//code from Stack Overflow to increase max limit to fix 413 error
+app.use(express.bodyParser({limit: '50mb'}));
 //code from https://enable-cors.org/server_expressjs.html
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
