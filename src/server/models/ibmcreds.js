101f854291df5591c6ef8afc7963203cf13eb104
Ryan Hill <rdh35@duke.edu>
2018-04-28 13:21:22 -0400

    making sure ibmcreds move gets pushed

---
+++ src/server/models/ibmcreds.js
@@ -0,0 +1,6 @@
+export const IBM_CREDENTIALS = {
+	"url": "https://gateway.watsonplatform.net/natural-language-understanding/api",
+    "username": "94af00e3-0c19-4043-907a-b3bd94601884",
+    "password": "Xuabov1uEGVI",
+	"version": "2018-03-16"
+};
